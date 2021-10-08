b58083867a361fa190500d8c433898213faf55d0
DARIO-ELIUDELIU <>
2021-10-08 14:29:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// FaZe el Sabriton
+// El Sabriton es mejor que sabritas
+var miNombre = "Darex";
+console.log("Bienvenido a Grove street families");
+console.log(miNombre);
+// En la gloria de la union sovietica, habian nazis infiltrados que experimentaban con amongus
+const esteAnio = 2021;
+console.log("Estamos en el " + esteAnio);
+
+// esteAnio = 6666; esto no se puede
+
+// Amongus
+// Atrapa al impostor
+// Investiga
+// has misiones
+// Juega limpio 
+
+console.log(15 + 20);
+console.log(1991 - 1945);
+console.log(5 + 2200);
+console.log("division");
+console.log(1 / 2);
+
+
+
+
+/* Ejercicio */
+// primero vamos a 
+// La salida de la consola debe ser la siguiente 
+
+// La calificacion 1 es 9 
+
+// La calificacion 2 es 10
+// La calificacion 3 es 7.5
+// Por lo tanto el promedio es 8.8333
+
+// recopilar las calificaciones 
+var cali1 = 10, cali2 = 7, cali3 = 8.25;
+// operaciones
+var suma = cali1 + cali2 + cali3;
+const nCalls = 3;
+var promedio = suma / nCalls;
+
+// mostrar resultados
+console.log("La Calificacion 1 es " + cali1);
+console.log("La Calificacion 2 es " + cali2);
+console.log("La Calificacion 3 es " + cali3);
+console.log("Por lo tanto el promedio es " + promedio);
+
+//fin
+
+/* 
+
+ */
+
+// recopilar las calificaciones
+var cali1 = 9.3, cali2 = 9.7, cali3 = 9.5;
+// operaciones
+var suma = cali1 + cali2 + cali3;
+const nCalls = 3;
+var promedio = suma / nCalls;
+
+// mostrar resultados
+console.log("La Calificacion 1 es " + cali1);
+console.log("La Calificacion 2 es " + cali2);
+console.log("La Calificacion 3 es " + cali3);
+console.log("Por lo tanto el promedio es " + promedio);
+
+//fin
